5d08e769b7413f560987954467ac841e5ed70db7
Pierre Pagnoux <pierre.pagnoux@algolia.com>
2020-12-08 15:38:45 +0100

    aoc8 - part2

--- 8/aoc8.go
+++ 8/aoc8.go
@@ -15,36 +15,64 @@ func max(a, b int) int {
 	return a
 }
 
+func unloop(program []string, executed []bool, pc int, patched bool) (bool, int) {
+	acc := 0
+	for pc < len(program) {
+		if executed[pc] {
+			fmt.Println(acc)
+			return false, acc
+		}
+
+		tokens := strings.Split(program[pc], " ")
+
+		switch tokens[0] {
+		case "nop":
+			if !patched {
+				programPatched := make([]string, len(program))
+				copy(programPatched, program)
+				executedCopy := make([]bool, len(executed))
+				programPatched[pc] = strings.Replace(programPatched[pc], "nop", "jmp", 1)
+				endReached, accRec := unloop(programPatched, executedCopy, pc, true)
+				if endReached {
+					return true, acc + accRec
+				}
+			}
+			executed[pc] = true
+			pc++
+		case "acc":
+			val, _ := strconv.Atoi(tokens[1])
+			acc += val
+			executed[pc] = true
+			pc++
+		case "jmp":
+			if !patched {
+				programPatched := make([]string, len(program))
+				copy(programPatched, program)
+				executedCopy := make([]bool, len(executed))
+				programPatched[pc] = strings.Replace(programPatched[pc], "jmp", "nop", 1)
+				endReached, accRec := unloop(programPatched, executedCopy, pc, true)
+				if endReached {
+					return true, acc + accRec
+				}
+			}
+			val, _ := strconv.Atoi(tokens[1])
+			executed[pc] = true
+			pc += val
+		}
+	}
+
+	return true, acc
+}
+
 func main() {
-	buffer, err := ioutil.ReadFile("input_aoc.txt")
+	buffer, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	str := strings.TrimSpace(string(buffer))
-
-	acc := 0
 	program := strings.Split(str, "\n")
 	executed := make([]bool, len(program))
-	for pc := 0; pc < len(program); {
-		if executed[pc] {
-			fmt.Println(acc)
-			break
-		}
 
-		tokens := strings.Split(program[pc], " ")
-
-		executed[pc] = true
-		switch tokens[0] {
-		case "nop":
-			pc++
-		case "acc":
-			val, _ := strconv.Atoi(tokens[1])
-			acc += val
-			pc++
-		case "jmp":
-			val, _ := strconv.Atoi(tokens[1])
-			pc += val
-		}
-	}
+	fmt.Println(unloop(program, executed, 0, false))
 }
